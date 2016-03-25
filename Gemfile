0e35aac3a6f9e4eca70560155b0918e49d26b2fc
vladiguez <ylannguez@gmail.com>
2016-03-25 19:22:44 +0100

    add a full_title helper

--- Gemfile
+++ Gemfile
@@ -44,6 +44,12 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
+group :test do
+  gem 'minitest-reporters', '1.0.5'
+  gem 'mini_backtrace',     '0.1.3'
+  gem 'guard', '~> 2.13'
+  gem 'guard-minitest',     '2.3.1'
+end
 group :production do
   gem 'pg',             '0.17.1'
   gem 'rails_12factor', '0.0.2'
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,24 +1,32 @@
 require 'test_helper'
 
+class ActiveSupport::TestCase
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical
+  # order.
+  fixtures :all
+
+  # Add more helper methods to be used by all tests here...
+end
+
 class StaticPagesControllerTest < ActionController::TestCase
-  def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
-  end
+  # def setup
+  #   @base_title = "Ruby on Rails Tutorial Sample App"
+  # end
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 end
